0d15002cb4fcabfaba76d1a25244af6d18f0aca6
Rastler <rastler89@gmail.com>
2024-10-17 18:40:23 +0200

    modificado efecto registro y notificación

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,18 +3,24 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { AuthService } from '../../_services/auth.service';
 import { NotificationService } from '../../_services/notification.service';
 import { Router, RouterLink } from '@angular/router';
+import { LoadingComponent } from '../../components/loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLink,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LoadingComponent
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  public isLoading: boolean = false;
 
   public formBuild = inject(FormBuilder);
   public formRegister: FormGroup = this.formBuild.group({
@@ -47,26 +53,28 @@ export class RegisterComponent {
       return;
     }
 
+    this.isLoading = true;
+
     this.authService.register(this.formRegister.value).subscribe({
       next: (data:any) => {
         this.notify.setAlert('Registro exitoso','success');
         this.router.navigate(['/login']);
+        this.isLoading = false;
       },
       error: (error: any) => {
         let errors = error.error.error;
 
         if(errors.name) {
-          this.notify.setAlert(errors.name[0],'error');
-          console.log(errors.name[0]);
+          this.notify.setAlert(errors.name[0],'danger');
         }
         if(errors.email) {
-          this.notify.setAlert(errors.email[0],'error');
-          console.log(errors.email[0]);
+          this.notify.setAlert(errors.email[0],'danger');
         }
         if(errors.password) {
-          this.notify.setAlert(errors.password[0],'error');
-          console.log(errors.password[0]);
+          this.notify.setAlert(errors.password[0],'danger');
         }
+
+        this.isLoading = false;
       }
     });
   }
