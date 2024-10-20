6006641e368c922208f9380c038d06e89dae141e
Rastler <rastler89@gmail.com>
2024-10-20 19:33:29 +0200

    visita

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { VisitComponent } from '../visit/visit.component';
 import { TreatmentComponent } from '../treatment/treatment.component';
 import { SurgeryComponent } from '../surgery/surgery.component';
 import { MedicalComponent } from '../medical/medical.component';
+import { Pet } from '../../interfaces/Pet';
 
 @Component({
   selector: 'app-history',
@@ -18,4 +19,10 @@ import { MedicalComponent } from '../medical/medical.component';
 })
 export class HistoryComponent {
   @Input() id!:string;
+  @Input() pet!:Pet;
+  @Output() update = new EventEmitter<void>();
+
+  actualizar() {
+    this.update.emit();
+  }
 }
--- src/app/components/visit/visit.component.ts
+++ src/app/components/visit/visit.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IsisterService } from '../../_services/isister.service';
+import { LoadingComponent } from '../loading/loading.component';
+import { NotificationService } from '../../_services/notification.service';
 
 @Component({
   selector: 'app-visit',
   standalone: true,
   imports: [
     CommonModule,
+    LoadingComponent,
     ReactiveFormsModule
   ],
   templateUrl: './visit.component.html',
@@ -15,14 +18,19 @@ import { IsisterService } from '../../_services/isister.service';
 })
 export class VisitComponent {
   @Input() id!: string;
+  @Input() visits: any;
+
+  @Output() update = new EventEmitter<void>();
+
   @ViewChild('closebutton') closebutton: any;
 
   visitForm: FormGroup;
-  public visits: any;
+  public isLoading: boolean = false;
 
   constructor(
     private isister: IsisterService,
-    private formBuild: FormBuilder
+    private formBuild: FormBuilder,
+    private notify: NotificationService
   ) {
     this.visitForm = this.formBuild.group({
       description: [''],
@@ -30,16 +38,10 @@ export class VisitComponent {
     })
   }
 
-  ngOnInit(): void {
-    this.isister.getVisit(this.id).subscribe({
-      next:(object:any) => {
-        this.visits = object;
-      }
-    })
-  }
-
   addVisit(): void {
     if(this.visitForm.invalid) return;
+
+    this.isLoading = true;
 
     const object:any = {
       description: this.visitForm.value.description,
@@ -49,9 +51,14 @@ export class VisitComponent {
     this.isister.addVisit(object,this.id).subscribe({
       next:(data:any) => {
         this.closebutton.nativeElement.click();
+        this.isLoading = false;
+        this.notify.setAlert('Visita añadida','success');
+        this.update.emit();
       },
       error:(error) => {
         console.log(error.status);
+        this.isLoading = false;
+        this.notify.setAlert('No se ha podido añadir','danger');
       }
     })
   }
