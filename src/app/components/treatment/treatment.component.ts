3091c1b967a1a46430319a06522300bdf4198b2c
Rastler <rastler89@gmail.com>
2024-10-20 20:16:21 +0200

    tratamientos

--- src/app/components/treatment/treatment.component.ts
+++ src/app/components/treatment/treatment.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IsisterService } from '../../_services/isister.service';
+import { LoadingComponent } from '../loading/loading.component';
+import { NotificationService } from '../../_services/notification.service';
 
 @Component({
   selector: 'app-treatment',
   standalone: true,
   imports: [
     CommonModule,
+    LoadingComponent,
     ReactiveFormsModule
   ],
   templateUrl: './treatment.component.html',
@@ -15,28 +18,25 @@ import { IsisterService } from '../../_services/isister.service';
 })
 export class TreatmentComponent {
   @Input() id!: string;
+  @Input() treatments: any;
+
+  @Output() update = new EventEmitter<any>();
+
   @ViewChild('closebutton') closebutton: any;
 
   treatmentForm: FormGroup;
-  public treatments: any;
+  public isLoading: boolean = false;
 
   constructor(
     private isister: IsisterService,
-    private formBuild: FormBuilder
+    private formBuild: FormBuilder,
+    private notify: NotificationService
   ) {
     this.treatmentForm = this.formBuild.group({
       description: [''],
       repetition: [''],
       start: [''],
       end: ['']
-    })
-  }
-
-  ngOnInit(): void {
-    this.isister.getTreatment(this.id).subscribe({
-      next:(object:any) => {
-        this.treatments = object;
-      }
     })
   }
 
@@ -50,12 +50,19 @@ export class TreatmentComponent {
       end: this.treatmentForm.value.end
     }
 
+    this.isLoading = true;
+
     this.isister.addTreatment(object,this.id).subscribe({
       next:(data:any) => {
         this.closebutton.nativeElement.click();
+        this.isLoading = false;
+        this.notify.setAlert('Tratamiento añadido','success');
+        this.update.emit();
       },
       error:(error) => {
         console.log(error.status);
+        this.isLoading = false;
+        this.notify.setAlert('No se ha podido añadir','danger');
       }
     })
   }
