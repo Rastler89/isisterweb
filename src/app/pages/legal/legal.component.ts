1527163a0be26a5d59a36903c47bb8ad0afc304a
Dani Molina (Rastler) <rastler89@gmail.com>
2024-11-05 12:16:29 +0100

    agregado footer

--- src/app/pages/legal/legal.component.ts
+++ src/app/pages/legal/legal.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderPublicComponent } from '../../components/header-public/header-public.component';
+import { FooterPublicComponent } from "../../components/footer-public/footer-public.component";
 
 @Component({
   selector: 'app-legal',
   standalone: true,
   imports: [
-    HeaderPublicComponent
-  ],
+    HeaderPublicComponent,
+    FooterPublicComponent
+],
   templateUrl: './legal.component.html',
   styleUrl: './legal.component.css'
 })
--- src/app/pages/privacity/privacity.component.ts
+++ src/app/pages/privacity/privacity.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderPublicComponent } from "../../components/header-public/header-public.component";
+import { FooterPublicComponent } from "../../components/footer-public/footer-public.component";
 
 @Component({
   selector: 'app-privacity',
   standalone: true,
-  imports: [HeaderPublicComponent],
+  imports: [HeaderPublicComponent, FooterPublicComponent],
   templateUrl: './privacity.component.html',
   styleUrl: './privacity.component.css'
 })
